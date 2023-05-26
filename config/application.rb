7f3238ea9a392800ef3581f25f73f1ede6ea279c
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-26 18:31:47 -0300

    chore: adiciona gem dotenv-rails para carregar variáveis de ambiente

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require 'action_cable/engine'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Dotenv::Railtie.load
 module RaroAcademyBank
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
