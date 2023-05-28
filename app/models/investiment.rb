43f5c6df0ed4b8406362e3de0ff321430ab9d800
klauslube <klauslube93@gmail.com>
2023-05-27 21:10:08 -0300

    feat: adiciona validaçoes iniciais dos atributos

--- app/models/investiment.rb
+++ app/models/investiment.rb
@@ -1,2 +1,5 @@
 class Investiment < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 1, maximum: 15 }
+  validates :minimum_amount, :profit, presence: true, numericality: { greater_than: 0 }
+  validates :premium, :income, :expiration_date, presence: true
 end
