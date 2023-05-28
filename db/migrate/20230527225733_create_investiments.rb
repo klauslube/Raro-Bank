7aea106eacf805c8b08cddf712c8d8ed5d9c585c
klauslube <klauslube93@gmail.com>
2023-05-27 21:06:43 -0300

    feat: adiciona modificadores da entidade

--- db/migrate/20230527225733_create_investiments.rb
+++ db/migrate/20230527225733_create_investiments.rb
@@ -1,12 +1,12 @@
 class CreateInvestiments < ActiveRecord::Migration[7.0]
   def change
     create_table :investiments do |t|
-      t.string :name
-      t.decimal :minimum_amount
-      t.decimal :income
-      t.boolean :premium
-      t.decimal :profit
-      t.date :expiration_date
+      t.string :name, null: false, limit: 30
+      t.decimal :minimum_amount, null:false, precision: 9, scale: 2
+      t.decimal :income, null: false, precision: 9, scale: 4
+      t.boolean :premium, null: false
+      t.decimal :profit, null: false, precision: 9, scale: 2
+      t.date :expiration_date, null: false
 
       t.timestamps
     end
