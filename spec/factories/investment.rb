31e3b00415642861ec79bfc768cc6223f4432779
klauslube <klauslube93@gmail.com>
2023-05-27 22:36:08 -0300

    feat: criação de um factory para Investment

---
+++ spec/factories/investment.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :investment do
+    name { Faker::Lorem.word }
+    minimum_amount { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
+    income { Faker::Number.decimal(l_digits: 4) }
+    premium { false }
+    profit { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
+    expiration_date { Faker::Date.forward(days: 30) }
+
+    trait :premium do
+      premium { true }
+    end
+  end
+end
