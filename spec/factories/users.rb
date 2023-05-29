1f9612bc69578a12ef1635e284bfb0a5638d132d
klauslube <klauslube93@gmail.com>
2023-05-29 15:19:07 -0300

    refactor: adiçao de role no factory bot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,14 @@ FactoryBot.define do
     cpf { Faker::IDNumber.brazilian_cpf }
     email { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 8, special_characters: true) }
+    role { :premium }
+
+    trait :admin do
+      role { :admin }
+    end
+
+    trait :admin do
+      role { :free }
+    end
   end
 end
