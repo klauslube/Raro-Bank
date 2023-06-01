8af9723c5613553f4aec77552e0d7893828cdab0
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-06-01 17:40:56 -0300

    feat: configura controle de rotas usando constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,49 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # Routes for devise user authentication
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root to: "home#index"
+  # Authenticated routes
+  authenticated :user do
+    # Routes for admin user (role: admin)
+    constraints(RoleConstraint.new([:admin])) do
+      root to: "admin#index", as: :admin_root #TODO: implement /admin route to redirect to admin#index
+      get '/admin', to: 'admin#index'
+
+      namespace :admin do
+        resources :classrooms
+        resources :users
+        resources :investments
+      end
+    end
+
+    # Routes for other users (role: free, premium)
+    constraints(RoleConstraint.new([:free, :premium])) do
+      root to: "home#index" #TODO: implement home#index
+
+      resources :transactions
+      resources :investments
+    end
+  end
+
+  # Unauthenticated routes
+  unauthenticated do
+    as :user do
+      root to: "devise/sessions#new", as: :unauthenticated_root
+    end
+
+    # All routes unauthenticated should go to root
+    get "*path", to: redirect("/")
+
+    # except for the ones below
+    get "/users/sign_in", to: "devise/sessions#new", as: :new_user_session_path
+    post "/users/sign_in", to: "devise/sessions#create", as: :user_session_path
+    get "/users/sign_up", to: "devise/registrations#new", as: :new_user_registration_path
+    post "/users", to: "devise/registrations#create", as: :user_registration_path
+    get "/users/password/new", to: "devise/passwords#new", as: :new_user_password_path
+    get "/users/password/edit", to: "devise/passwords#edit", as: :edit_user_password_path
+    patch "/users/password", to: "devise/passwords#update", as: :user_password_path
+    post "/users/password", to: "devise/passwords#create"
+  end
 end
