e32108863c1a55abf662f83723125a619d235eaa
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-06-01 17:50:59 -0300

    test: adiciona testes de controller para Home

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,82 @@
+require "rails_helper"
+
+RSpec.describe HomeController, type: :controller do
+  context "Unauthenticated" do
+    it "should raise error: not free or premium user" do
+        # Simula um usuário não autenticado
+        allow(controller).to receive(:authenticate_free_or_premium_user!).and_raise(StandardError.new("not_a_free_or_premium_user"))
+
+        # Executa a ação GET no método index
+        expect { get :index }.to raise_error(StandardError, "not_a_free_or_premium_user")
+    end
+
+    it 'should respond with 302' do
+      get :index
+      expect(response).to have_http_status(302)
+    end
+
+    it 'should redirect to login' do
+      get :index
+      expect(response).to redirect_to('/')
+    end
+  end
+
+  describe "#index" do
+    context "Authenticated as free user" do
+      let(:user) { create(:user_confirmed) }
+
+      before do
+        sign_in user
+      end
+
+      it "should respond with 200" do
+        get :index
+        # byebug
+        expect(response).to have_http_status(200)
+      end
+
+      it "should render index template" do
+        get :index
+        expect(response).to render_template(:index)
+      end
+    end
+
+    context "Authenticated as premium user" do
+      let(:user) { create(:user_confirmed, :premium) }
+
+      before do
+        sign_in user
+      end
+
+      it "should respond with 200" do
+        get :index
+        # byebug
+        expect(response).to have_http_status(200)
+      end
+
+      it "should render index template" do
+        get :index
+        expect(response).to render_template(:index)
+      end
+    end
+
+    context "Unauthorized as admin user" do
+      let(:admin) { create(:user, :admin) }
+
+      before do
+        sign_in admin
+      end
+
+      it "should respond with 302" do
+        get :index
+        # byebug
+        expect(response).to have_http_status(302)
+      end
+
+      it "should be redirect to login" do
+        get :index
+        expect(response).to redirect_to('/users/sign_in')
+      end
+    end
+  end
+end
