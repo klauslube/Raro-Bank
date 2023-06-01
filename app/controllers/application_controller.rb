77966e4184f23f7ce1308c6692f52d8bed53aebd
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-06-01 17:44:33 -0300

    feat: implementa funções de auth para cada user 'role'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,33 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protected
+
+  def authenticate_admin!
+    return if current_user&.admin?
+
+    flash[:alert] = :not_an_admin
+    redirect_to root_path
+  end
+
+  def authenticate_free_user!
+    return if current_user&.free?
+
+    flash[:alert] = :not_a_free_user
+    redirect_to root_path
+  end
+
+  def authenticate_premium_user!
+    return if current_user&.premium?
+
+    flash[:alert] = :not_a_premium_user
+    redirect_to root_path
+  end
+
+  def authenticate_free_or_premium_user!
+    return if current_user && (current_user.free? || current_user.premium?)
+
+    flash[:alert] = :not_a_free_or_premium_user
+    redirect_to root_path
+  end
 end
