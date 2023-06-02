3927a4e72425e54a635de44e7733ab456d56d8c7
klauslube <klauslube93@gmail.com>
2023-06-02 18:00:59 -0300

    test: implementa teste de #show

--- spec/controllers/investments_controller_spec.rb
+++ spec/controllers/investments_controller_spec.rb
@@ -1,14 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Admin::InvestmentsController, type: :controller do
-  let(:admin) { create(:user_confirmed, role: :admin) }
+  describe "Authenticated as admin user" do
+    let(:admin) { create(:user_confirmed, role: :admin) }
+    let(:investment) { create(:investment)}
+    before :each do
+      sign_in admin
+    end
 
-  before :each do
-    sign_in admin
-  end
+    context '#index' do
+      it 'should respond with 200' do
+        get :index
+        expect(response).to have_http_status(200)
+      end
 
-  it 'should respond with 200' do
-    get :index
-    expect(response).to have_http_status(200)
+      it 'renders the index template' do
+        get :index
+        expect(response).to render_template(:index)
+      end
+    end
+
+    context '#show' do
+      it 'should respond with 200' do
+        get :show, params: { id: investment.id }
+        expect(response).to have_http_status(200)
+      end
+
+      it 'renders the show template' do
+        get :show, params: { id: investment.id }
+        expect(response).to render_template(:show)
+      end
+    end 
   end
 end
