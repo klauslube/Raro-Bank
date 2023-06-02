dd20b70c5f7244998f6e7c4e667bd1332497d257
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-06-01 22:03:05 -0300

    test: ajusta nomes dos testes de rotas não autenticadas

--- spec/routes/unauthenticated_spec.rb
+++ spec/routes/unauthenticated_spec.rb
@@ -1,25 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'Routes', type: :routing do
-   describe 'Unauthenticated' do
-    it 'should routes to devise sessions#new' do
+  describe 'when user not authenticated' do
+    it 'should route be available devise sessions#new' do
       expect(get: '/users/sign_in').to route_to('devise/sessions#new')
     end
 
-    it 'should routes to devise sessions#create' do
+    it 'should route be available devise sessions#create' do
       expect(post: '/users/sign_in').to route_to('devise/sessions#create')
     end
 
-    it 'should routes to devise registrations#new' do
+    it 'should route be available devise registrations#new' do
       expect(get: '/users/sign_up').to route_to('devise/registrations#new')
     end
 
-    it 'should routes to devise registrations#create' do
+    it 'should route be available devise registrations#create' do
       expect(post: '/users').to route_to('devise/registrations#create')
     end
 
-    it 'should routes to devise passwords#new' do
+    it 'should route be available devise passwords#new' do
       expect(get: '/users/password/new').to route_to('devise/passwords#new')
     end
   end
 end
+
