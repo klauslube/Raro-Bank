6fa4dff85fc3688ec06d7b101d82acccf1aa100d
klauslube <klauslube93@gmail.com>
2023-06-02 18:02:07 -0300

    feat: seed para Investment

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,7 @@ free.save!
 premium = User.new name: 'User Premium', cpf: '00033300011', email: 'premium@rarobank.com', password: 'r@robank123', role: 2
 premium.skip_confirmation!
 premium.save!
+
+investment = Investment.new name: 'Selic', minimum_amount: 100, income: 0.1, premium: true, profit: 110, expiration_date: DateTime.tomorrow, approver_id: '7f49e628-01d5-4c94-b196-9e322ed03479'
+investment.skip_confirmation!
+investment.save!
