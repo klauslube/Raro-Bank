fe5d4bf4ee877d4a2786c5f1dea148cda1c0808a
klauslube <klauslube93@gmail.com>
2023-06-05 23:19:15 -0300

    feat: criaçao de service e metodos para importar dados da api e salvar no db

--- db/migrate/20230605232306_add_column_date_to_indicator.rb
+++ db/migrate/20230605232306_add_column_date_to_indicator.rb
@@ -1,5 +1,5 @@
 class AddColumnDateToIndicator < ActiveRecord::Migration[7.0]
   def change
-    add_column :indicators, :rate_date, :date, null: false
+    add_column :indicators, :rate_date, :date
   end
 end
