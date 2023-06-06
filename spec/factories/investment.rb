8e1917610f98ecf12821406d69598b1eaedd74d2
klauslube <klauslube93@gmail.com>
2023-06-06 17:06:39 -0300

    refactor: ajustes no factory com nova adiçao de atributo

--- spec/factories/investment.rb
+++ spec/factories/investment.rb
@@ -2,12 +2,13 @@ FactoryBot.define do
   factory :investment do
     name { Faker::Lorem.word }
     minimum_amount { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
-    income { Faker::Number.decimal(l_digits: 4) }
     premium { false }
     expiration_date { Faker::Date.forward(days: 30) }
     approver_id { FactoryBot.create(:user, role: :admin).id }
+    # indicator_id { FactoryBot.create(:indicator, name: 'SELIC').id }
 
     association :approver, factory: :user
+    association :indicator, factory: :indicator, id: 1
 
     trait :premium do
       premium { true }
