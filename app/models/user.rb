b3248b5764175d5d92f61d5d1c007be31ccd0191
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-06-06 04:43:02 -0300

    feat: adiciona novos scopes para users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
   has_one :account, dependent: :destroy
+  has_many :user_investments, dependent: :destroy
   belongs_to :classroom, optional: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
-
-  has_many :user_investments, dependent: :destroy
 
   enum :role, {
     free: 1,
@@ -13,21 +12,22 @@ class User < ApplicationRecord
     admin: 3
   }, scopes: true, default: :free
 
-  validates :name, :cpf, :email, :password, presence: true
+  validates :name, :email, :cpf, presence: true
+  validates :password, presence: true, on: :create
   validates :cpf, length: { is: 11 }, uniqueness: true, numericality: { only_integer: true }
   validates :name, length: { minimum: 3, maximum: 255 }
-  validates :cpf, length: { is: 11 }
 
   validate :strong_password
 
   scope :by_cpf, ->(cpf) { where(cpf:) }
   scope :by_name, ->(name) { where(name:) }
+  scope :by_email, ->(email) { where(email:) }
+  scope :all_except_admins, -> { where.not(role: :admin) }
   scope :name_contains, ->(contain) { where('name ILIKE ?', "%#{contain}%") }
   scope :unconfirmed_email, -> { where(confirmed_at: nil) }
   scope :confirmed_email, -> { where.not(confirmed_at: nil) }
 
   after_create :create_account
-  before_destroy :check_if_last_admin
 
   private
 
@@ -37,13 +37,6 @@ class User < ApplicationRecord
     errors.add(:password, :no_letter) unless password =~ /[a-zA-Z]/
     errors.add(:password, :no_number) unless password =~ /\d/
     errors.add(:password, :no_special) unless password =~ /[!@#$%^&*()\-_=+{};:,<.>]/
-  end
-
-  def check_if_last_admin
-    return unless role == 'admin' && User.admin.count == 1
-
-    errors.add(:base, 'Cannot delete the last admin user.')
-    throw(:abort)
   end
 
   def create_account
