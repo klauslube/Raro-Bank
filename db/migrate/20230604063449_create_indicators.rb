2dc811596d01d3c460262302f8dcca0767d2b74f
klauslube <klauslube93@gmail.com>
2023-06-05 23:02:45 -0300

    feat: criaçao de tablea Indicator

--- db/migrate/20230604063449_create_indicators.rb
+++ db/migrate/20230604063449_create_indicators.rb
@@ -1,8 +1,8 @@
 class CreateIndicators < ActiveRecord::Migration[7.0]
   def change
     create_table :indicators do |t|
-      t.decimal :rate, precision: 9, scale: 6, null: false
-      t.string :web_address, null: false
+      t.decimal :rate, precision: 10, scale: 6, null: false
+      t.string :name, null: false
       
       t.timestamps
     end
