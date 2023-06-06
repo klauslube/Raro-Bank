5fd3ef25757519fda5dee9ebd3858c0fbe821c56
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-06-05 23:58:43 -0300

    feat: adiciona factory bot

--- spec/factories/classroom.rb
+++ spec/factories/classroom.rb
@@ -3,5 +3,7 @@ FactoryBot.define do
     name { Faker::Educator.course_name }
     start_date { Faker::Date.backward(days: 30) }
     end_date { Faker::Date.forward(days: 30) }
+
+    user_ids { [] }
   end
 end
