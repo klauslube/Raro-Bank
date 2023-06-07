80a490241e7c19c2a29e272bfc3d4a216579b740
klauslube <klauslube93@gmail.com>
2023-06-07 00:26:01 -0300

    test: testes refatorados para passarem com novas modificações de atributos

--- spec/factories/investment.rb
+++ spec/factories/investment.rb
@@ -3,9 +3,10 @@ FactoryBot.define do
     name { Faker::Lorem.word }
     minimum_amount { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
     premium { false }
+    start_date { Date.current }
     expiration_date { Faker::Date.forward(days: 30) }
     approver_id { FactoryBot.create(:user, role: :admin).id }
-    # indicator_id { FactoryBot.create(:indicator, name: 'SELIC').id }
+    indicator_id { FactoryBot.create(:indicator).id }
 
     association :approver, factory: :user
     association :indicator, factory: :indicator, id: 1
