1cd74f6e8df96147ab64b7e14ed4757b259f0c29
klauslube <klauslube93@gmail.com>
2023-06-07 17:24:32 -0300

    feat: criado validaçao para verificar se valor aplicado é maior que o minimo

--- app/models/user_investment.rb
+++ app/models/user_investment.rb
@@ -3,6 +3,11 @@ class UserInvestment < ApplicationRecord
   belongs_to :investment
 
   validates :initial_amount, presence: true, numericality: { greater_than: 0 }
+  validate :initial_amount_enough?
 
-  scope :by_user, ->(user) { where(user:) }
+  private
+
+  def initial_amount_enough?
+    errors.add(:initial_amount, 'needs to be more than minimum amount') if initial_amount < investment.minimum_amount
+  end
 end
