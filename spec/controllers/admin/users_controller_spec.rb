0c57f85a7c4c407dda1b43d8b4a05afee3435a77
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-06-07 11:39:10 -0300

    refactor: retira comentario desnecessarios

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,14 +1,14 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Admin::UsersController, type: :controller do
-  describe "logged in as admin" do
+  describe 'logged in as admin' do
     let(:admin) { create(:user_confirmed, role: :admin) }
 
     before :each do
       sign_in admin
     end
 
-    context "GET #index" do
+    context 'GET #index' do
       let(:user) { create(:user_confirmed) }
       let(:premium_user) { create(:user_confirmed, role: :premium) }
 
@@ -16,83 +16,83 @@ RSpec.describe Admin::UsersController, type: :controller do
         get :index
       end
 
-      it "should assigns all users but current_user to @users" do
+      it 'should assigns all users but current_user to @users' do
         expect(assigns(:users)).to match_array([user, premium_user])
       end
 
-      it "should respond with 200" do
+      it 'should respond with 200' do
         expect(response).to have_http_status(200)
       end
 
-      it "should renders the index template" do
+      it 'should renders the index template' do
         get :index
         expect(response).to render_template(:index)
       end
 
-      it "should not include current_user" do
+      it 'should not include current_user' do
         expect(assigns(:users)).not_to include(admin)
       end
     end
 
-    context "GET #edit" do
+    context 'GET #edit' do
       let(:user) { create(:user_confirmed) }
 
       before :each do
         get :edit, params: { id: user.id }
       end
 
-      it "should assigns the requested user to @user" do
+      it 'should assigns the requested user to @user' do
         expect(assigns(:user)).to eq(user)
       end
 
-      it "should respond with 200" do
+      it 'should respond with 200' do
         expect(response).to have_http_status(200)
       end
 
-      it "should renders the edit template" do
+      it 'should renders the edit template' do
         expect(response).to render_template(:edit)
       end
     end
 
-    context "PATCH #update" do
+    context 'PATCH #update' do
       let(:user) { create(:user_confirmed) }
 
-      context "with valid parameters" do
-        it "should assigns the requested user to @user" do
+      context 'with valid parameters' do
+        it 'should assigns the requested user to @user' do
           patch :update, params: { id: user.id, user: attributes_for(:user_confirmed) }
 
           expect(assigns(:user)).to eq(user)
         end
 
-        it "should update user" do
-          patch :update, params: { id: user.id, user: attributes_for(:user_confirmed, name: "New Name") }
+        it 'should update user' do
+          patch :update, params: { id: user.id, user: attributes_for(:user_confirmed, name: 'New Name') }
 
           user.reload
 
-          expect(user.name).to eq("New Name")
+          expect(user.name).to eq('New Name')
         end
 
-        it "should redirect to admin_users_path" do
+        it 'should redirect to admin_users_path' do
           patch :update, params: { id: user.id, user: attributes_for(:user_confirmed) }
 
           expect(response).to redirect_to(admin_users_path)
         end
 
-        it "should set flash[:notice]" do
+        it 'should set flash[:notice]' do
           patch :update, params: { id: user.id, user: attributes_for(:user_confirmed) }
 
-          expect(flash[:notice]).to eq("User updated successfully.")
+          expect(flash[:notice]).to eq('User updated successfully.')
         end
       end
 
-      context "with invalid parameters" do
-        it "should assigns the requested user to @user" do
+      context 'with invalid parameters' do
+        it 'should assigns the requested user to @user' do
           patch :update, params: { id: user.id, user: attributes_for(:user_confirmed, name: nil) }
 
           expect(assigns(:user)).to eq(user)
         end
 
-        it "should not update user" do
+        it 'should not update user' do
           old_name = user.name
 
           patch :update, params: { id: user.id, user: attributes_for(:user_confirmed, name: nil) }
@@ -114,11 +114,10 @@ RSpec.describe Admin::UsersController, type: :controller do
         end
 
         it 'should updates the admin user' do
-
           expect(admin.reload.name).to eq('New Name')
         end
 
-        it "should redirect to admin_users_path" do
+        it 'should redirect to admin_users_path' do
           expect(response).to redirect_to(admin_users_path)
         end
 
@@ -153,11 +152,10 @@ RSpec.describe Admin::UsersController, type: :controller do
         end
 
         it 'should destroy the admin user' do
-          # debugger
           expect(User.exists?(current_user.id)).to eq(false)
         end
 
-        it "should redirect to admin_users_path" do
+        it 'should redirect to admin_users_path' do
           expect(response).to redirect_to(root_path)
         end
 
