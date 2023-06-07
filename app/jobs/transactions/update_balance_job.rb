ce2704d695c76c1b619c6b39b6a0d83af60474a6
NoByte-dev <nobyte_dev@hotmail.com>
2023-06-07 06:49:21 -0300

    feat: Valida e agenda data/hora de uma transferência

---
+++ app/jobs/transactions/update_balance_job.rb
@@ -0,0 +1,34 @@
+module Transactions
+  class UpdateBalanceJob < ApplicationJob
+    queue_as :transactions
+
+    sidekiq_options retry: 5
+    def perform(transaction_id)
+      transaction = Transaction.find(transaction_id)
+      Transactions::UpdateBalanceService.new(transaction).call if within_transfer_hours?
+      next_business_day(transaction)
+    end
+
+    private
+
+    def within_transfer_hours?
+      current_time = Time.now.in_time_zone('America/Fortaleza')
+      weekday = current_time.wday
+      hour = current_time.hour
+      weekday >= 1 && weekday <= 5 && hour >= 7 && hour < 18
+    end
+
+    def next_business_day(transaction)
+      next_transfer_day = find_next_transfer_day
+      next_transfer_time = next_transfer_day.beginning_of_day + 7.hours
+      self.class.set(wait_until: next_transfer_time).perform_later(transaction.id)
+    end
+
+    def find_next_transfer_day
+      current_day = Time.now.in_time_zone('America/Fortaleza').to_date
+      next_day = current_day + 1.day
+      next_day += 1.day until next_day.on_weekday?
+      next_day
+    end
+  end
+end
