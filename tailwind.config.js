3c6cfce3b7992dd7b29fd9d554fe255d1e525a19
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-06-07 22:05:12 -0300

    chore: instala stimulus notification

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  plugins: [require("flowbite/plugin"), require("@tailwindcss/forms")],
+};
