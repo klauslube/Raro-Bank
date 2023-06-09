997970d31ae92347a874bcdadb090a82a4b78847
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-06-08 23:00:09 -0300

    feat: adiciona envio de saldo para turmas

--- app/controllers/admin/transactions_controller.rb
+++ app/controllers/admin/transactions_controller.rb
@@ -1,19 +1,33 @@
 module Admin
   class TransactionsController < ApplicationController
-    before_action :fetch_receiver, only: %i[create]
     before_action :fetch_amount, only: %i[create]
+    before_action :fetch_classroom, only: %i[create fetch_classroom_users_accounts]
+    before_action :fetch_classroom_users_accounts, only: %i[create]
 
     def new
       @transaction = Transaction.new
     end
 
     def create
-      transaction = Transaction.new(sender: current_user.account, receiver: @receiver, amount: @amount, token: generate_token)
+      receivers = []
+      errors = []
 
-      if transaction.save
-        redirect_to root_path, notice: t('.success')
+      if @classroom_users.present?
+        receivers = @classroom_users
       else
-        redirect_to admin_deposits_path, alert: t('.failure')
+        receiver = fetch_receiver
+        receivers << receiver
+      end
+
+      receivers.each do |receiver_account|
+        transaction = Transaction.new(sender: current_user.account, receiver: receiver_account, amount: @amount, token: generate_token)
+        errors << transaction.errors.full_messages.to_sentence unless transaction.save
+      end
+
+      if errors.empty?
+        redirect_to admin_root_path, notice: t('.success')
+      else
+        flash[:alert] = t('.failure') + "- receiver: #{receiver_account} -" + errors.join(' ')
       end
     end
 
@@ -23,13 +37,25 @@ module Admin
       SecureRandom.hex(10)
     end
 
+    def fetch_classroom
+      @classroom = Classroom.find_by(id: params[:transaction][:classroom_id])
+    end
+
+    def fetch_classroom_users_accounts
+      return if @classroom.nil?
+
+      @classroom_users = @classroom.users.map(&:account)
+      return @classroom_users unless @classroom_users.empty?
+    end
+
     def fetch_receiver
       receiver_cpf = params[:transaction][:receiver_cpf]
-      @receiver = User.find_by(cpf: receiver_cpf)&.account
+      receiver = User.find_by(cpf: receiver_cpf)&.account
 
-      return @receiver unless @receiver.nil?
+      return receiver unless receiver.nil?
 
       redirect_to admin_deposits_path, alert: t('.cpf_error')
+      nil
     end
 
     def fetch_amount
