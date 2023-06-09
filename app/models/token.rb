d5013ea1aa5b1b5034582216a7699b70799e1f36
NoByte-dev <nobyte_dev@hotmail.com>
2023-06-09 07:54:40 -0300

    feat: Scope de tokens ativos

--- app/models/token.rb
+++ app/models/token.rb
@@ -2,4 +2,6 @@ class Token < ApplicationRecord
   belongs_to :transfer, class_name: 'Transaction', foreign_key: :transaction_id, dependent: :destroy, inverse_of: :token
 
   validates :code, presence: true
+
+  scope :tokens_actives, -> { where(active: true) }
 end
