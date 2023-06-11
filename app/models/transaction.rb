a82f66872a8db4310594ba7dca0c31085e0fa318
klauslube <klauslube93@gmail.com>
2023-06-11 20:53:38 -0300

    fix: envio de email apenas quando criar uma transfer

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -12,7 +12,7 @@ class Transaction < ApplicationRecord
   after_create :token_countdown
   after_create :cancel_transfer_countdown
   after_commit :update_balance
-  after_commit :new_transfer
+  after_commit :new_transfer, on: :create
 
   enum :status, {
     started: 1,
