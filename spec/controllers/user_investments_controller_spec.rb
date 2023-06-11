0db1363e620052297b2560dc61dd88a6dfe23527
klauslube <klauslube93@gmail.com>
2023-06-11 19:24:49 -0300

    test: criaçao de testes para action index e catalogs

--- spec/controllers/user_investments_controller_spec.rb
+++ spec/controllers/user_investments_controller_spec.rb
@@ -1,69 +1,37 @@
-# require 'rails_helper'
+RSpec.describe UserInvestmentsController, type: :controller do
+  describe 'index action' do
+    let(:user) { create(:user, role: :premium) }
+    let!(:account) { create(:account, user: user, balance: 1000) }
+    let!(:user_investments) { create_list(:user_investment, 3, user: user, initial_amount: 200) }
 
-# RSpec.describe UserInvestmentsController, type: :controller do
-#   let(:free) { create(:user_confirmed, role: :free) }
-#   let(:premium) { create(:user_confirmed, role: :premium) }
-#   let(:investment) { create(:investment)}
-#   let(:free_investment) { create(:investment, premium: false) }
-#   let(:premium_investment) { create(:investment, premium: true) }
-#   let(:free_user_investment) { create(:user_investment, user: free, investment: free_investment) }
-#   let(:premium_user_investment) { create(:user_investment, user: premium, investment: premium_investment) }
 
-#   # let(:valid_attributes) { attributes_for(:investment) }
-#   # let(:invalid_attributes) { attributes_for(:investment, name: nil) }
-#   describe 'Action #index' do
-   
-#     context 'when user is free' do
-#       before :each do
-#         sign_in free
-#       end
+    before { sign_in(user) }
 
-#       it "assigns only free user investments to user_investments" do
-#         expect(assigns(:user_investments)).to eq([free_user_investment])
-#       end
-  
-#       it 'renders the index template' do
-#         get :index
-#         expect(response).to render_template(:index)
-#       end
-#     end
-  
-#     context "when user is premium" do
-#       before :each do
-#         sign_in premium
-#       end
-  
-#       # it "assigns all user investments to @user_investments" do
-#       #   expect(assigns(:user_investments)).to eq([free_user_investment, premium_user_investment])
-#       # end
-  
-#       it "renders the index template" do
-#         expect(response).to render_template(:index)
-#       end
-#     end
-#   end
+    it 'assigns user_investments' do
+      get :index
+      expect(assigns(:user_investments)).to match_array(user_investments)
+    end
 
-#   describe 'Action #new' do
-#     it "assigns a new user investment to @user_investment" do
-#       expect {
-#         post :create, params: { user_investment: valid_attributes }
-#       }.to change(Investment, :count).by(1)
-#     end
-  
-#     it "assigns all investments to @investments" do
-#       investments = Investment.all
-#       get :new
-#       expect(assigns(:investments)).to eq(investments)
-#     end
-  
-#     it "returns a success response" do
-#       get :new
-#       expect(response).to be_successful
-#     end
-  
-#     it "renders the new template" do
-#       get :new
-#       expect(response).to render_template(:new)
-#     end
-#   end
-# end
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'catalogs action' do
+    let(:user) { create(:user) }
+    let!(:investments) { create_list(:investment, 3) }
+
+    before { sign_in(user) }
+
+    it 'assigns investments' do
+      get :catalogs
+      expect(assigns(:investments)).to match_array(investments)
+    end
+
+    it 'renders the catalogs template' do
+      get :catalogs
+      expect(response).to render_template(:catalogs)
+    end
+  end
+end
